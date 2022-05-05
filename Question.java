b8f347db652bcd37365b861584fa5f8d54d4ae2a
Shad95 <48450177+Shad95@users.noreply.github.com>
2022-05-05 17:13:25 +0530

    Add files via upload
    initial commit

---
+++ Question.java
@@ -0,0 +1,55 @@
+package com.assignment.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+@Entity
+@Table(name= "question_table")
+public class Question {
+    @Id
+	private long qId;
+	private String question;
+	private String questionCategory;
+	@JsonFormat(pattern = "dd-MM-yyyy" , shape  = Shape.STRING)
+	@Column(name = "created_on")
+	private String createdOn;
+	private String createdBy;
+	public long getqId() {
+		return qId;
+	}
+	public void setqId(long qId) {
+		this.qId = qId;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getQuestionCategory() {
+		return questionCategory;
+	}
+	public void setQuestionCategory(String questionCategory) {
+		this.questionCategory = questionCategory;
+	}
+	public String getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(String createdOn) {
+		this.createdOn = createdOn;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	
+	
+	
+}
